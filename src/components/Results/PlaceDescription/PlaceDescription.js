5c8608b5faf8beb15499287d2288e49cb5925766
mandeep2809 <mandeepkandhari@gmail.com>
2018-11-08 14:14:48 -0500

    Removing the directions button

--- src/components/Results/PlaceDescription/PlaceDescription.js
+++ src/components/Results/PlaceDescription/PlaceDescription.js
@@ -14,7 +14,6 @@ class PlaceDescription extends Component{
 				<div>
 					<img src={this.props.photos} alt='place image'  height={200} width={400}/>
 				</div>
-				<button className='w-30 pa2 mt1 ml1 mb0 ba b--black bw1 bg-white grow pointer fw6 f6 dib link'>directions</button>
 				<section className='w-90 pa1 mv2 flex flex-column items-start tl'>
 					<h4 className='mv0 fw5 grow'>{this.props.name}</h4>
 					<div className='w-90 f6 grow flex items-center justify-between mv0 '>
