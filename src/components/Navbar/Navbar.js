087610852f8d69acd3cb0accd75ac5f2a9fe6bbe
mandeep2809 <mandeepkandhari@gmail.com>
2018-11-08 17:45:34 -0500

    Changing the background color of the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,12 +5,12 @@ import './Navbar.css';
 class Navbar extends Component{
 	render(){
 		return(
-			<div className='center flex items-center justify-center pa2 bg-light-green'>
+			<div className='center flex items-center justify-center pa2 navbackground'>
 				<div className='w-30 pt2 rotate-vert-center center'>
 					<img src={Logo} alt='Google Map Logo' width={100} height={100}/>
 				</div>
 				<div className='w-70 pt2'>
-					<h1 className='f2 mid-gray'>Place Finder</h1>
+					<h1 className='f2 light-yellow'>Place Finder</h1>
 				</div> 
 			</div>
 		);
